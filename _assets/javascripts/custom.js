c2bf156e20361492e3bce2e677b5c0feec252e17
vonavi <ivvl82@gmail.com>
2014-04-28 19:25:28 +0300

    Show all the game text, not only PGN notation.

--- _assets/javascripts/custom.js
+++ _assets/javascripts/custom.js
@@ -16,18 +16,6 @@ function customFunctionOnPgnGameLoad() {
 }
 function customFunctionOnMove() {
   refreshButtonset();
-
-  /* Mark commented moves */
-  for (var Ply = 1; Ply <= PlyNumberVar[0]; Ply++) {
-    if (strippedMoveComment(Ply, 0, true))
-      $('#Var0Mv' + Ply).addClass('bg-info');
-  }
-
-  /* Show move comment */
-  if (CurrentVar == 0 && (theObj = document.getElementById("GameMoveComment"))) {
-    theObj.innerHTML = '<span class="comment">' +
-      strippedMoveComment(CurrentPly, 0, true) + '</span>';
-  }
 }
 function refreshButtonset() {
   $('#GameButtons input').addClass('btn btn-default');
