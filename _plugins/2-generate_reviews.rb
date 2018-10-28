003de0bbb1478cf88eb0b7d48a143ec30f2a3eee
Vladimir Hodakov <vladimir@hodakov.me>
2018-10-28 20:51:58 +0400

    Fix CI error (4)

--- _plugins/2-generate_reviews.rb
+++ _plugins/2-generate_reviews.rb
@@ -4,7 +4,20 @@ module Jekyll
     safe true
     priority :low
 
-    include I18nFilter
+    LOCALE = :ru
+
+    def localize(input, format=nil)
+      load_translations
+      format = (format =~ /^:(\w+)/) ? $1.to_sym : format
+      I18n.l input, :format => format
+    end
+
+    def load_translations
+      unless I18n::backend.instance_variable_get(:@translations)
+        I18n.backend.load_translations Dir[File.join(File.dirname(__FILE__), '../_locales/*.yml')]
+        I18n.locale = LOCALE
+      end
+    end
 
     # Generates the reviews file
     def generate(site)
